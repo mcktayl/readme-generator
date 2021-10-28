c0d025a17ce5ef310e89041f0a64a8f3f93e240c
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-10-28 17:48:58 -0400

    linked function to create readme document when prompts are finished

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const gm = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the link to the deployed application?',
+        name: 'link',
+    },
+    {
+        type: 'input',
+        message: 'What is a short description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What are the instructions and some examples of use for your project?',
+        name: 'usage',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+        fs.writeFileSync(fileName, gm(data))
+    };
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => writeToFile('README.md', data))
+    .then(() => console.log('Successfully wrote file'))
+    .catch((err) => console.error(err))
+    };
 
 // Function call to initialize app
 init();
